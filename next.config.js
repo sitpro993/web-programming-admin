0b85e7f85cc61220e2b0b928f56e2d95a605618f
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2022-02-25 15:00:22 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = {
     domains: ["res.cloudinary.com"],
   },
   env: {
-    //API_URL: "https://web-programming-api.herokuapp.com",
-    API_URL: "http://localhost:5000",
+    API_URL: "https://web-programming-api.herokuapp.com",
+    //API_URL: "http://localhost:5000",
     BASE_URL: "http://localhost:3001",
     CLOUDINARY_URL:
       "cloudinary://948981829889381:2949tnH2B_D54DJIjZiXqdClhR4@beeyou",
